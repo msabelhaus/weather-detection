6f6bc9f0106b5322679811e8c657083b28b0c7de
Margaret Sabelhaus <margaret.sabelhaus@flyfrontier.com>
2023-11-20 17:15:21 -0700

    Adding CNN code

---
+++ src/models/cnn.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import os
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Define the CNN model
+def create_cnn_model(input_shape=(256, 256, 3), num_classes=10):
+    model = models.Sequential()
+    
+    # Convolutional layers
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    # Fully connected layers
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    # Compile the model
+    model.compile(optimizer='adam', 
+                  loss='categorical_crossentropy', 
+                  metrics=['accuracy'])
+
+    return model
+
+# Set the paths to your preprocessed dataset
+train_data_dir = os.path.abspath("../../data/processed/train")
+validation_data_dir = os.path.abspath("../../data/processed/test")
+
+# Image data generators for training and validation
+train_datagen = ImageDataGenerator()
+validation_datagen = ImageDataGenerator()
+
+# Batch size and image dimensions
+batch_size = 32
+input_shape = (256, 256, 3)
+
+# Flow training images in batches using the generators
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=input_shape[:2],
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=input_shape[:2],
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+
+# Number of classes
+num_classes = len(train_generator.class_indices)
+
+# Create the CNN model
+cnn_model = create_cnn_model(input_shape=input_shape, num_classes=num_classes)
+
+# Train the model
+epochs = 10
+history = cnn_model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size
+)
+
+
+# Save the model using Pickle
+if not os.path.exists(os.path.abspath("../models")):
+    os.makedirs(os.path.abspath("../models"))    
+
+pickle_path = os.path.abspath("../models/cnn.pkl")
+with open(pickle_path, 'wb') as file:
+    pickle.dump(cnn_model, file)
+
+
